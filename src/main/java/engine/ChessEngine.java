7d8e776408cd1caac6b49b0ad0ff8092c7bbf819
Niflheim <David-Wang@Outlook.com>
2018-02-10 19:33:47 -0600

    ChessEngine Interface update

--- src/main/java/engine/ChessEngine.java
+++ src/main/java/engine/ChessEngine.java
@@ -1,4 +1,17 @@
 package engine;
 
 public interface ChessEngine {
+    boolean start();
+
+    void stop();
+
+    String reset();
+
+    String getBestMove(String fen, int difficulty);
+
+    String getCheckers(String fen);
+
+    String getLegalMoves(String fen);
+
+    String makeMove(String fen, String pgn);
 }
